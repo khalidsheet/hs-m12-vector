5996afd961f7ea6811e316c028f3d00456230960
Khalid Mohammad <khalid.m.sheet@gmail.com>
2023-06-18 15:13:14 +0300

    Added missing methods

--- Vector.js
+++ Vector.js
@@ -6,8 +6,8 @@ class Vector {
 
   /**
    * Instasiate a new vector with the given x and y values
-   * @param {*} x First vector
-   * @param {*} y Second vector
+   * @param {Number} x First vector
+   * @param {Number} y Second vector
    * @returns New instance of Vector
    */
   static of(x, y) {
@@ -16,7 +16,7 @@ class Vector {
 
   /**
    * Add a vector to this vector
-   * @param {*} vector Vector to add
+   * @param {Vector} vector Vector to add
    * @returns New instance of Vector
    * @throws Error if vector is not a Vector
    **/
@@ -34,6 +34,50 @@ class Vector {
    */
   abs() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  /**
+   * Substract a vector from this vector
+   * @param {Vector} vector Vector to substract
+   * @returns New instance of Vector
+   * @throws Error if vector is not a Vector
+   **/
+  substract(vector) {
+    if (!(vector instanceof Vector)) {
+      throw new Error("substract() expects a Vector as parameter");
+    }
+
+    return Vector.of(this.x - vector.x, this.y - vector.y);
+  }
+
+  /**
+   * Multiply this vector by a scalar
+   * @param {Number} scalar Scalar to multiply by
+   * @returns New instance of Vector
+   * @throws Error if scalar is not a number
+   * */
+  multiply(scalar) {
+    if (typeof scalar !== "number") {
+      throw new Error("multiply() expects a number as parameter");
+    }
+
+    return Vector.of(this.x * scalar, this.y * scalar);
+  }
+
+  /**
+   * Find the unit vector of this vector
+   * @returns New instance of Vector
+   * */
+  unit() {
+    return this.multiply(1 / this.abs());
+  }
+
+  /**
+   * Find the direction angle of this vector
+   * @returns The direction angle of the vector
+   * */
+  angle() {
+    return Math.atan2(this.y, this.x);
   }
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const Vector = require("./Vector");
 
-const a = new Vector(1, 2);
+const a = new Vector(3, 3);
 
 const b = Vector.of(2, 0.5);
 
 const sum = a.add(b);
 
-console.log(sum);
+console.log({ sum });
 
-console.log(sum.abs());
+console.log({ sum: sum.abs() });
+
+const diff = a.substract(b);
+
+console.log({ diff });
+
+const mult = a.multiply(2);
+
+console.log({ mult });
+
+const unit = a.unit();
+
+console.log({ unit });
+
+const angle = a.angle();
+
+console.log({ angle });
